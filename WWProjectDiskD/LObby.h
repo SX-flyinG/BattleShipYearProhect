62b7c896d179c9754e50533e1f8170b7a3746062
SX-flyinG <outcast9n0k@gmail.com>
2024-12-06 17:26:32 +0200

    Update LObby.h

--- WWProjectDiskD/LObby.h
+++ WWProjectDiskD/LObby.h
@@ -1,7 +1,7 @@
 
 void rules();
 void accInfo(); 
-void lobby();
+void lobby(int& countOfGames , int& countOfWins);
 void Tutorial();
 void Play(int& countOfGames , int& countOfWins);
 
@@ -11,7 +11,7 @@ void rules() {
 
 	cout << "Setup : " << endl;
 	cout << "Players : Battleship is a game for two players. " << endl;
-	cout << "Grid : Each player has two grids : one for placing their own ships, and another for tracking hits and misses on the opponent’s grid.Each grid is typically 10x10 squares, labeled with letters(A - J) for columns and numbers(1 - 10) for rows." << endl;
+	cout << "Grid : Each player has two grids : one for placing their own ships, and another for tracking hits and misses on the opponentâ€™s grid.Each grid is typically 10x10 squares, labeled with letters(A - J) for columns and numbers(1 - 10) for rows." << endl;
 	cout << "Ships : Each player has a fleet of ships to place on their grid.The typical fleet consists of : " << endl;
 	cout << "\t1)Battleship(4 squares)" << endl;
 	cout << "\t2)Submarine(3 squares)" << endl;
@@ -23,14 +23,14 @@ void rules() {
 	cout << "Turns : " << endl;
 	cout << "Players take turns calling out coordinates(for example, B5) to attack their opponent's grid. " << endl;
 	cout << "Hit or Miss :  " << endl;
-	cout << "If a player’s shot hits an opponent's ship, the opponent must announce a hit and mark the hit spot on their grid. " << endl;
+	cout << "If a playerâ€™s shot hits an opponent's ship, the opponent must announce a hit and mark the hit spot on their grid. " << endl;
 	cout << "If the shot misses, the opponent announces a miss, and the player marks that spot as missed on their tracking grid. " << endl;
 	cout << "Sinking Ships : " << endl;
 	cout << "When all squares of a ship are hit, that ship is considered sunk. The player who owns the ship must announce, You sunk my [type of ship]!" << endl;
 	cout << "Tracking : " << endl;
 	cout << "Each player should track their shots and the opponent's hits to avoid targeting the same spot twice.  " << endl;
 	cout << "Winning the Game : " << endl;
-	cout << "The first player to sink all of their opponent’s ships wins the game." << endl;
+	cout << "The first player to sink all of their opponentâ€™s ships wins the game." << endl;
 	int choice;
 	cout << "Return to lobby : ";
 	cin >> choice;
@@ -53,7 +53,14 @@ void accInfo(int& countOfGames , int& countOfWins) {
 	outFileName.close();
 	cout << "Username :  " << username << endl;
 	cout << "Titul : " << titul << endl;
-	cout << "Winrate : " << (countOfWins / countOfGames) * 100 << endl;
+	if (countOfGames != 0){
+		cout << "Winrate : " << (countOfWins / countOfGames) * 100 << endl;
+	}
+	else {
+		cout << "Winrate : 0%"  << endl;
+	}
+	
+	
 	delete[]titul;
 	titul = nullptr;
 	lobby();
@@ -70,7 +77,7 @@ void lobby() {
 		rules();
 	}
 	else if (choice == 1) {
-		accInfo();
+		accInfo(countOfGames , countOfWins);
 	}
 	else if (choice == 2) {
 		Tutorial();
